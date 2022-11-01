cf1df58e2478a1537f40f4ecf7537bcbbb348bc6
Anthony Stevens <aastevens1126@gmail.com>
2022-11-01 12:21:02 -0500

    servlets exercise

--- src/main/java/HelloWorldServlet.java
+++ src/main/java/HelloWorldServlet.java
@@ -1,9 +1,23 @@
+import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+@WebServlet(name = "TestServlet", urlPatterns = "/hello")
 public class HelloWorldServlet extends HttpServlet {
     protected void doGet(HttpServletRequest req, HttpServletResponse res) {
+        res.setContentType("text/html");
+        String name = req.getParameter("name");
+
+        try {
+            if(name == null){
+                res.getWriter().println("<h1> Hello World</h1>");
+            }else{
+                res.getWriter().println("<h1>"+name+" Hello World</h1>");
+            }
+        }catch (Exception e){
+            e.printStackTrace();
+        }
 
     }
 }
